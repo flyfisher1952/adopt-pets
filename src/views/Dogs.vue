195aac282dc9478d3df23cdc95abfbc35b57ff5b
Mike Lockhart <mike@lockhart.consulting>
2020-06-24 13:12:29 -0600

    25% through

---
+++ src/views/Dogs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Dogs for Adoption</h1>
+    <b-table striped hover :items="dogs">
+      <template slot="name" slot-scope="data">
+        <a :href="`#${data.value.replace(/[^a-z]+/i, '-').toLowerCase()}`">
+          {{ data.value }}</a
+        >
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import dogs from '@/data/dogs'
+export default {
+  data () {
+    return {
+      dogs
+    }
+  }
+}
+</script>
