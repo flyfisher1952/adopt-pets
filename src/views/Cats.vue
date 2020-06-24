d8c188823ba111d1838ec13de85d2348864f2ce6
Mike Lockhart <mike@lockhart.consulting>
2020-06-24 13:13:10 -0600

    25% through

---
+++ src/views/Cats.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Cats for Adoption</h1>
+    <b-table striped hover :items="cats">
+        <template slot="name" slot-scope="data">
+            <a :href="`#${data.value.replace(/[^a-z]+/i, '-').toLowerCase()}`"> {{ data.value }}</a>
+        </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import cats from '@/data/cats'
+export default {
+  data () {
+    return {
+      cats
+    }
+  }
+}
+</script>
---
+++ src/views/Pet.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Pets for Adoption</h1>
+    <b-table striped hover :items="pets" />
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import cats from '@/data/cats'
+import dogs from '@/data/dogs'
+
+export default {
+  data () {
+    return {
+      cats: cats,
+      dogs: dogs,
+      pets: _.sortBy(_.concat(dogs, cats), p => {
+        return p.name
+      })
+    }
+  }
+}
+</script>
